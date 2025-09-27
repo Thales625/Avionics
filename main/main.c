a0fa3d673f2c6629fe4060139eb37bc3945abdd8
thales <thaleshend@gmail.com>
2025-09-27 14:41:04 -0300

    update CMakeLists and pin config

--- main/main.c
+++ main/main.c
@@ -67,8 +67,8 @@ void app_main(void) {
                             (1ULL << LED_PIN) | 
                             (1ULL << BUZZER_PIN),
             .mode = GPIO_MODE_OUTPUT,
-            .pull_up_en = 0,
-            .pull_down_en = 0,
+            .pull_up_en = GPIO_PULLUP_DISABLE,
+            .pull_down_en = GPIO_PULLDOWN_DISABLE,
             .intr_type = GPIO_INTR_DISABLE
         };
         ESP_ERROR_CHECK(gpio_config(&io_conf));
