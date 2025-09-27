09a099cb9f1ad495a3999908272b93bbc4d85a85
thales <thaleshend@gmail.com>
2025-09-27 17:50:35 -0300

    add sync method and fix identation

--- components/sdcard/sdcard.h
+++ components/sdcard/sdcard.h
@@ -7,6 +7,7 @@ esp_err_t sdcard_list_files(void);
 esp_err_t sdcard_open_file(const char *path, const char *mode, FILE **file_out);
 esp_err_t sdcard_read_file(const char *path);
 esp_err_t sdcard_write(const char *data, FILE *file_ptr);
+esp_err_t sdcard_sync(FILE *file_ptr);
 esp_err_t sdcard_clear_file(const char *path);
 esp_err_t sdcard_delete_file(const char *path);
 esp_err_t sdcard_close_file(FILE* file_ptr);
