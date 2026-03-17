ed6a29f23a2b2c75a11649e394acb5a7db636960
thales <thaleshend@gmail.com>
2026-03-17 19:28:25 -0300

    decouple flight logic and add simulation support

--- tools/Simulation/avionics.h
+++
@@ -1,9 +0,0 @@
-#ifndef AVIONICS_H
-#define AVIONICS_H
-
-#include <inttypes.h>
-
-void setup(float pressure, float accel, uint32_t time_s);
-void loop(float pressure, float accel, uint32_t time_s);
-
-#endif
