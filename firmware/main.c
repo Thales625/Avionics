6bda8c3966e800d1abecae2fca004a45f55692ec
thales <thaleshend@gmail.com>
2026-03-20 04:00:10 -0300

    firmware: improve error handling and task organization

--- firmware/main.c
+++ firmware/main.c
@@ -6,7 +6,9 @@
 #include "driver/gpio.h"
 #include "driver/i2c.h"
 
+#include <math.h>
 #include <stdio.h>
+#include <inttypes.h>
 
 #include "sdcard.h"
 #include "mpu6050.h"
@@ -34,11 +36,10 @@ static const char* TAG = "avionics";
 
 static bmp280_t bmp_dev = { 0 };
 static mpu6050_dev_t mpu_dev = { 0 };
-static FILE *file_ptr;
 
 static uint32_t ut_sd_sync;
 static flight_logic_t flight_logic;
-// char text[128];
+static char text[128];
 
 static void beep(uint32_t duration) {
     gpio_set_level(BUZZER_PIN, 0);
@@ -46,7 +47,23 @@ static void beep(uint32_t duration) {
     gpio_set_level(BUZZER_PIN, 1);
 }
 
-inline static vector3f_t mpu_convert_accel(mpu6050_acceleration_t accel) {
+static void avionics_abort(int code) {
+    // set safe state
+    gpio_set_level(PARACHUTE_PIN, 0);
+    gpio_set_level(LED_PIN, 0);
+    gpio_set_level(BUZZER_PIN, 1);
+
+    // abort loop
+    while (1) {
+        for (int i=0; i<code; i++) {
+            beep(100);
+            vTaskDelay(pdMS_TO_TICKS(200));
+        }
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+static inline vector3f_t mpu_convert_accel(mpu6050_acceleration_t accel) {
     return (vector3f_t) {
         .x = accel.x,
         .y = accel.y,
@@ -54,7 +71,7 @@ inline static vector3f_t mpu_convert_accel(mpu6050_acceleration_t accel) {
     };
 }
 
-inline static vector3f_t mpu_convert_gyro(mpu6050_rotation_t rot) {
+static inline vector3f_t mpu_convert_gyro(mpu6050_rotation_t rot) {
     return (vector3f_t) {
         .x = rot.x,
         .y = rot.y,
@@ -62,12 +79,107 @@ inline static vector3f_t mpu_convert_gyro(mpu6050_rotation_t rot) {
     };
 }
 
-void app_main(void) {
-    // read sensors variables
+static void avionics_task(void *arg) {
+    // sensor aux variables
     mpu6050_acceleration_t mpu_acc = { 0 };
     mpu6050_rotation_t mpu_gyro = { 0 };
 
-    // config GPIO
+    float bmp_temperature = 0;
+    float bmp_pressure = 0;
+    
+    // log file
+    FILE *file_ptr;
+
+    // startup delay + buzzer indication
+    gpio_set_level(LED_PIN, 0);
+    vTaskDelay(pdMS_TO_TICKS(8000));
+    beep(500);
+
+    // SDCARD: open or create log file
+    if (sdcard_open_file(SD_FILE, "w", &file_ptr) != ESP_OK) {
+        sdcard_umount();
+        ESP_LOGE(TAG, "Failed to open/create file");
+        avionics_abort(2);
+    }
+
+    // startup indication
+    gpio_set_level(LED_PIN, 1);
+    vTaskDelay(pdMS_TO_TICKS(5000)); // wait 5 sec
+    beep(300);
+
+    // init flight logic core
+    flight_logic.sensor_data.ut = (uint32_t)(esp_timer_get_time() / 1000ULL);
+    flight_logic_init(&flight_logic);
+    ut_sd_sync = flight_logic.sensor_data.ut;
+
+    // main loop
+    while (1) {
+        flight_logic.sensor_data.ut = (uint32_t)(esp_timer_get_time() / 1000ULL);
+
+        // MPU6050: read data
+        if (mpu6050_get_motion(&mpu_dev, &mpu_acc, &mpu_gyro) != ESP_OK) {
+            ESP_LOGW(TAG, "MPU6050: read failed");
+
+            if (flight_logic.state == STATE_PRE_FLIGHT) {
+                avionics_abort(3);
+            }
+
+            flight_logic.sensor_data.accel.x = NAN;
+            flight_logic.sensor_data.accel.y = NAN;
+            flight_logic.sensor_data.accel.z = NAN;
+
+            flight_logic.sensor_data.rot.x = NAN;
+            flight_logic.sensor_data.rot.y = NAN;
+            flight_logic.sensor_data.rot.z = NAN;
+        } else {
+            flight_logic.sensor_data.accel = mpu_convert_accel(mpu_acc);
+            flight_logic.sensor_data.rot = mpu_convert_gyro(mpu_gyro);
+        }
+
+        // BMP280: read data
+        if (bmp280_read_float(&bmp_dev, &bmp_temperature, &bmp_pressure) != ESP_OK) {
+            ESP_LOGW(TAG, "BMP280: read failed");
+
+            if (flight_logic.state == STATE_PRE_FLIGHT) {
+                avionics_abort(3);
+            }
+        } else {
+            flight_logic.sensor_data.temperature = bmp_temperature;
+            flight_logic.sensor_data.pressure = bmp_pressure;
+        }
+        
+        // update flight logic
+        flight_logic_update(&flight_logic);
+
+        // SDCARD: write
+        snprintf(text, sizeof(text),
+            "%d %" PRIu32 " %.4f %.4f %.4f %.4f %.4f %.4f %.4f %.2f\n",
+            flight_logic.state,
+            flight_logic.sensor_data.ut,
+            flight_logic.sensor_data.accel.x,
+            flight_logic.sensor_data.accel.y,
+            flight_logic.sensor_data.accel.z,
+            flight_logic.sensor_data.rot.x,
+            flight_logic.sensor_data.rot.y,
+            flight_logic.sensor_data.rot.z,
+            flight_logic.sensor_data.pressure,
+            flight_logic.sensor_data.temperature
+        );
+		sdcard_write(text, file_ptr);
+
+        // SDCARD: sync
+        if (flight_logic.sensor_data.ut - ut_sd_sync > SD_SYNC_INTERVAL) {
+            sdcard_sync(file_ptr);
+            ut_sd_sync = flight_logic.sensor_data.ut;
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+    vTaskDelete(NULL);
+}
+
+void app_main(void) {
+    // GPIO configuration
     {
         gpio_config_t in_conf = {
             .pin_bit_mask = (1ULL << PBUTTON_PIN),
@@ -88,19 +200,15 @@ void app_main(void) {
             .intr_type = GPIO_INTR_DISABLE
         };
         ESP_ERROR_CHECK(gpio_config(&out_conf));
-
-        gpio_set_level(PARACHUTE_PIN, 0);
-        gpio_set_level(LED_PIN, 1);
-        gpio_set_level(BUZZER_PIN, 1);
     }
 
-    // I2C
+    // I2C initialization
     {
         ESP_ERROR_CHECK(i2cdev_init());
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 
-    // BMP280
+    // BMP280 initialization
     {
         bmp280_params_t params;
         bmp280_init_default_params(&params);
@@ -116,10 +224,10 @@ void app_main(void) {
         );
         vTaskDelay(pdMS_TO_TICKS(200));
         ESP_ERROR_CHECK(bmp280_init(&bmp_dev, &params));
-        ESP_LOGI(TAG, "Found BMP280 device");
+        ESP_LOGI(TAG, "Found BMP280");
     }
 
-    // MPU6050
+    // MPU6050 initialization
     {
         ESP_ERROR_CHECK(
             mpu6050_init_desc(
@@ -131,87 +239,27 @@ void app_main(void) {
             )
         );
         vTaskDelay(pdMS_TO_TICKS(200));
-        esp_err_t res;
-        while (1) {
-            res = i2c_dev_probe(&mpu_dev.i2c_dev, I2C_DEV_WRITE);
-            if (res == ESP_OK) {
-                ESP_LOGI(TAG, "Found MPU6050 device");
-                break;
-            }
-            ESP_LOGE(TAG, "MPU6050 not found");
-            vTaskDelay(pdMS_TO_TICKS(1000));
-        }
-        vTaskDelay(pdMS_TO_TICKS(200));
         ESP_ERROR_CHECK(mpu6050_init(&mpu_dev));
         ESP_ERROR_CHECK(mpu6050_set_full_scale_gyro_range(&mpu_dev, MPU6050_GYRO_RANGE_500));
         ESP_ERROR_CHECK(mpu6050_set_full_scale_accel_range(&mpu_dev, MPU6050_ACCEL_RANGE_4));
+        ESP_LOGI(TAG, "Found MPU6050");
     }
 
-    // SDCARD
+    // SDCARD initialization
     {
         if (sdcard_init(SD_MOSI_PIN, SD_MISO_PIN, SD_SCLK_PIN, SD_CS_PIN) != ESP_OK) {
-            beep(5000);
-            vTaskDelay(pdMS_TO_TICKS(1000));
-
-            sdcard_umount();
             ESP_LOGE(TAG, "Failed to initialize SD card");
-            return;
+
+            beep(5000);
+            avionics_abort(1);
         }
     }
 
-    // SETUP
-    gpio_set_level(LED_PIN, 0);
-    vTaskDelay(pdMS_TO_TICKS(8000));
-    beep(500);
-
-    // WAIT BUTTON
-    while (gpio_get_level(PBUTTON_PIN)) vTaskDelay(pdMS_TO_TICKS(200));
-
-    // SDCARD: open/create file
-    if (sdcard_open_file(SD_FILE, "w", &file_ptr) != ESP_OK) {
-        sdcard_umount();
-        ESP_LOGE(TAG, "Failed to open/create file");
-        return;
-    }
-
-    // ABORT CHECK
-    if (bmp280_read_float(&bmp_dev, &flight_logic.sensor_data.temperature, &flight_logic.sensor_data.pressure) != ESP_OK || mpu6050_get_motion(&mpu_dev, &mpu_acc, &mpu_gyro) != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to read sensors");
-        return;
-    } 
-
-    // INDICATE STARTUP
+    // set default values
+    gpio_set_level(PARACHUTE_PIN, 0);
     gpio_set_level(LED_PIN, 1);
-    vTaskDelay(pdMS_TO_TICKS(5000)); // wait 5 sec
-    beep(300);
+    gpio_set_level(BUZZER_PIN, 1);
 
-    // INIT FLIGHT LOGIC
-    flight_logic.sensor_data.ut = (uint32_t)(esp_timer_get_time() / 1000ULL);
-    flight_logic_init(&flight_logic);
-    ut_sd_sync = flight_logic.sensor_data.ut;
-
-    // main loop
-    while (1) {
-        flight_logic.sensor_data.ut = (uint32_t)(esp_timer_get_time() / 1000ULL);
-
-        // read mpu6050
-        ESP_ERROR_CHECK(mpu6050_get_motion(&mpu_dev, &mpu_acc, &mpu_gyro));
-        flight_logic.sensor_data.accel = mpu_convert_accel(mpu_acc);
-        flight_logic.sensor_data.rot = mpu_convert_gyro(mpu_gyro);
-
-        // read bmp280
-        ESP_ERROR_CHECK(bmp280_read_float(&bmp_dev, &flight_logic.sensor_data.temperature, &flight_logic.sensor_data.pressure));
-        
-        // write sdcard
-		// snprintf(text, sizeof(text), "%d %ld %.4f %.4f %.4f %.4f %.4f %.4f %.4f %.2f\n", flight_state, ut, accel.x, accel.y, accel.z, rotation.x, rotation.y, rotation.z, pressure, temperature);
-		// sdcard_write(text, file_ptr);
-
-        // sync sdcard
-        if (flight_logic.sensor_data.ut - ut_sd_sync > SD_SYNC_INTERVAL) {
-            sdcard_sync(file_ptr);
-            ut_sd_sync = flight_logic.sensor_data.ut;
-        }
-
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
+    // create avionics task
+    xTaskCreate(avionics_task, "avionics_task", 4096, NULL, 10, NULL);
 }
