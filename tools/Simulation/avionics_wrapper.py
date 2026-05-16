89d459dccd518b30ed5b90954942beb1e0630d5c
thales <thaleshend@gmail.com>
2026-05-16 20:50:25 -0300

    simulation: minor updates

--- tools/Simulation/avionics_wrapper.py
+++ tools/Simulation/avionics_wrapper.py
@@ -7,7 +7,7 @@ class AvionicsSim:
 
         # log callback
         def logger(msg):
-            print(f"[{self._core.sensor_data.ut:04}] {msg}")
+            print(f"[{self._core.state.ut:04}] {msg}")
         self._cb = _lib.sim_log_callback_t(logger)
         _lib.flight_logic_set_sim_logger(self._cb)
 
@@ -18,25 +18,25 @@ class AvionicsSim:
         _lib.flight_logic_update(ctypes.byref(self._core))
 
     def set_sensors(self, ut, ax, ay, az, rx, ry, rz, press, temp):
-        self._core.sensor_data.ut = int(ut)
-        self._core.sensor_data.accel.x = float(ax)
-        self._core.sensor_data.accel.y = float(ay)
-        self._core.sensor_data.accel.z = float(az)
-        self._core.sensor_data.rot.x = float(rx)
-        self._core.sensor_data.rot.y = float(ry)
-        self._core.sensor_data.rot.z = float(rz)
-        self._core.sensor_data.pressure = float(press)
-        self._core.sensor_data.temperature = float(temp)
+        self._core.state.ut = int(ut)
+        self._core.state.accel.x = float(ax)
+        self._core.state.accel.y = float(ay)
+        self._core.state.accel.z = float(az)
+        self._core.state.ang_vel.x = float(rx)
+        self._core.state.ang_vel.y = float(ry)
+        self._core.state.ang_vel.z = float(rz)
+        self._core.state.pressure = float(press)
+        self._core.state.temperature = float(temp)
 
     # getters
     @property
     def state(self): return self._core.state
-    
+
     @property
     def altitude(self): return self._core.altitude_baro
-    
+
     @property
     def trigger_parachute(self): return self._core.trigger_parachute
-    
+
     @property
     def trigger_shutdown(self): return self._core.trigger_shutdown
