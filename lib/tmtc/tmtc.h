409cee7e920f612c4915b5b82d096fae9af4c04a
thales <thaleshend@gmail.com>
2026-05-19 02:42:51 -0300

    firmware: telemetry divided into lora and flash

--- lib/tmtc/tmtc.h
+++ lib/tmtc/tmtc.h
@@ -11,31 +11,64 @@
 
 #define TMTC_CHANNEL 65
 
-// downlink
+// uplink
+typedef struct __attribute__((packed)) {
+    uint8_t id;
+    uint32_t param;
+} telecommand_payload_t;
+
 typedef struct __attribute__((packed)) {
     uint32_t magic;
 
+    telecommand_payload_t payload;
+
+    uint16_t checksum;
+} telecommand_packet_t;
+
+// downlink
+typedef struct __attribute__((packed)) {
     uint32_t ut;
+
+    float accel_mag;
+    float ang_vel_mag;
+    float pressure;
+    float temperature;
+    float altitude;
+
+    int32_t lat_nmea, lon_nmea;
+    uint8_t satellites;
+
     uint8_t phase;
+} lora_payload_t;
+
+typedef struct __attribute__((packed)) {
+    uint32_t magic;
+
+    lora_payload_t payload;
+
+    uint16_t checksum;
+} lora_packet_t;
+
+// flash memory
+typedef struct __attribute__((packed)) {
+    uint32_t ut;
+
     vector3f_t accel;
     vector3f_t ang_vel;
     float pressure;
     float temperature;
+
     int32_t lat_nmea, lon_nmea;
     uint8_t satellites;
 
-    uint16_t checksum;
-} telemetry_packet_t;
+    uint8_t phase;
+} flash_payload_t;
 
-// uplink
 typedef struct __attribute__((packed)) {
     uint32_t magic;
 
-    uint8_t id;
-    uint32_t param;
-
-    uint16_t checksum;
-} telecommand_packet_t;
+    flash_payload_t payload;
+} flash_packet_t;
 
 // CRC-16-CCITT
 uint16_t crc16(const uint8_t *data, size_t length);
